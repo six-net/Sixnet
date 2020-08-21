aebc372def8e58c78ed001f912e25b1eadb8c4bf
DingBin.Li <lidingbin@live.com>
2020-08-21 16:45:28 +0800

    'modify'

--- EZNEW/Extensions/TypeExtensions.cs
+++ EZNEW/Extensions/TypeExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace System
 {
@@ -12,26 +15,68 @@ namespace System
         /// </summary>
         static readonly Type AllowNullType = typeof(Nullable<>);
 
+        /// <summary>
+        /// Enum value and names
+        /// </summary>
+        static readonly ConcurrentDictionary<string, Dictionary<int, string>> CacheEnumValueAndNames = new ConcurrentDictionary<string, Dictionary<int, string>>();
+
         #region Generate dictionary by enum
 
         /// <summary>
         /// Generate dictionary by enum
         /// </summary>
         /// <param name="enumType">Enum type</param>
+        /// <param name="displayFriendly">Display friendly</param>
         /// <returns>Return the dictionary value</returns>
-        public static Dictionary<int, string> GetEnumValueAndNames(this Type enumType)
+        public static Dictionary<int, string> GetEnumValueAndNames(this Type enumType, bool displayFriendly = true)
         {
             if (enumType == null)
             {
                 return new Dictionary<int, string>(0);
             }
+
+            string formatedKey = $"{enumType.GUID}_{displayFriendly}";
+            if (CacheEnumValueAndNames.TryGetValue(formatedKey, out var valueAndNames))
+            {
+                return valueAndNames ?? new Dictionary<int, string>(0);
+            }
             var values = Enum.GetValues(enumType);
             Dictionary<int, string> enumValues = new Dictionary<int, string>();
+            var displayAttrType = typeof(DisplayAttribute);
             foreach (int val in values)
             {
-                enumValues.Add(val, Enum.GetName(enumType, val));
+                var enumName = Enum.GetName(enumType, val);
+                if (displayFriendly)
+                {
+                    var enumField = enumType.GetField(enumName);
+
+                    if (enumField != null && enumField.IsDefined(displayAttrType, false))
+                    {
+                        var displayName = (enumField.GetCustomAttributes(displayAttrType, false).First() as DisplayAttribute)?.Name;
+                        enumName = string.IsNullOrWhiteSpace(displayName) ? enumName : displayName;
+                    }
+                }
+                enumValues.Add(val, enumName);
             }
+            CacheEnumValueAndNames[formatedKey] = enumValues;
             return enumValues;
+        }
+
+        #endregion
+
+        #region Get enum display name
+
+        /// <summary>
+        /// Get enum display name
+        /// </summary>
+        /// <param name="enumValue">Enum value</param>
+        /// <returns></returns>
+        public static string GetEnumDisplayName(this Enum enumValue)
+        {
+            var enumType = enumValue.GetType();
+            var valueAndNames = GetEnumValueAndNames(enumType, true);
+            valueAndNames.TryGetValue(Convert.ToInt32(enumValue), out string displayName);
+            return displayName ?? enumValue.ToString();
         }
 
         #endregion
