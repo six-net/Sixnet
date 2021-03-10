d4855a05fcb24a500be999d1104f95601bbdf951
DingBin.Li <lidingbin@live.com>
2021-03-10 16:14:40 +0800

    modify

---
+++ EZNEW/Component/ComponentConfigureOptions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EZNEW.Component
+{
+    /// <summary>
+    /// Component configure options
+    /// </summary>
+    public class ComponentConfigureOptions
+    {
+        /// <summary>
+        /// Gets or sets the service collection
+        /// </summary>
+        public IServiceCollection Services { get; set; }
+    }
+}
---
+++ EZNEW/Component/IComponentConfigurationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EZNEW.Component
+{
+    /// <summary>
+    /// Component configuration service contract
+    /// </summary>
+    public interface IComponentConfigurationService
+    {
+        /// <summary>
+        /// Configure component
+        /// </summary>
+        /// <param name="componentConfigureOptions">Component configure </param>
+        void Configure(ComponentConfigureOptions componentConfigureOptions);
+    }
+}
