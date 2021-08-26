19e5be8ecb53ed2ac112f159b50f000a079d4c69
DingBin.Li <lidingbin@live.com>
2021-08-26 17:25:32 +0800

    modify

--- EZNEW/Drawing/VerificationCode/VerificationCodeProvider.cs
+++ EZNEW/Drawing/VerificationCode/VerificationCodeProvider.cs
@@ -12,29 +12,13 @@ namespace EZNEW.Drawing.VerificationCode
     {
         #region Fields
 
-        protected int length = 5;//code length
+        protected int lengthValue = 5;//code length
 
         protected int minLength = 1;//min length
 
         protected int maxLenght = 50;//max length
 
-        protected VerificationCodeType codeType = VerificationCodeType.NumberAndLetter;//type
-
-        protected int interfereNum = 3; //interfere num
-
         protected static readonly char[] charArray = { '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-
-        protected int fontSize = 30;
-
-        protected int spaceBetween = 1;
-
-        protected Color backgroundColor = Color.White;
-
-        protected Color? fontColor = null;//use random color if null
-
-        protected Color? interfereColor = null;//use random color if null
-
-        protected string frontFamilyName = "";//front family name
 
         protected static readonly Random random = new Random();
 
@@ -49,7 +33,7 @@ namespace EZNEW.Drawing.VerificationCode
         {
             get
             {
-                return length;
+                return lengthValue;
             }
             set
             {
@@ -60,126 +44,45 @@ namespace EZNEW.Drawing.VerificationCode
         /// <summary>
         /// Gets or sets the verification code type
         /// </summary>
-        public VerificationCodeType CodeType
-        {
-            get
-            {
-                return codeType;
-            }
-            set
-            {
-                codeType = value;
-            }
-        }
+        public VerificationCodeType CodeType { get; set; } = VerificationCodeType.NumberAndLetter;
 
         /// <summary>
         /// Gets or sets the font size
         /// </summary>
-        public int FontSize
-        {
-            get
-            {
-                return fontSize;
-            }
-            set
-            {
-                fontSize = value;
-            }
-        }
+        public int FontSize { get; set; } = 30;
 
         /// <summary>
         /// Gets or sets the char space
         /// </summary>
-        public int SpaceBetween
-        {
-            get
-            {
-                return spaceBetween;
-            }
-            set
-            {
-                spaceBetween = value;
-            }
-        }
+        public int SpaceBetween { get; set; } = 1;
 
         /// <summary>
         /// Gets or sets the background color.
         /// Use White by default.
         /// </summary>
-        public Color BackgroundColor
-        {
-            get
-            {
-                return backgroundColor;
-            }
-            set
-            {
-                backgroundColor = value;
-            }
-        }
+        public Color BackgroundColor { get; set; } = Color.White;
 
         /// <summary>
         /// Gets or sets the font color.
         /// Use random colors if set to null value.
         /// </summary>
-        public Color? FontColor
-        {
-            get
-            {
-                return fontColor;
-            }
-            set
-            {
-                fontColor = value;
-            }
-
-        }
+        public Color? FontColor { get; set; } = null;
 
         /// <summary>
         /// Gets or sets the interfere lines number
         /// </summary>
-        public int InterfereNum
-        {
-            get
-            {
-                return interfereNum;
-            }
-            set
-            {
-                interfereNum = value;
-            }
-        }
+        public int InterfereNum { get; set; } = 3;
 
         /// <summary>
         /// Gets or sets the interfere line color
         /// Use random colors if set to null value
         /// </summary>
-        public Color? InterfereColor
-        {
-            get
-            {
-                return interfereColor;
-            }
-            set
-            {
-                interfereColor = value;
-            }
-        }
+        public Color? InterfereColor { get; set; } = null;
 
         /// <summary>
         /// Gets or sets the front family name
         /// </summary>
-        public string FrontFamilyName
-        {
-            get
-            {
-                return frontFamilyName;
-            }
-            set
-            {
-                frontFamilyName = value;
-            }
-        }
+        public string FrontFamilyName { get; set; } = string.Empty;
 
         #endregion
 
@@ -199,7 +102,7 @@ namespace EZNEW.Drawing.VerificationCode
             }
             length = length < minLength ? minLength : length;
             length = length > maxLenght ? maxLenght : length;
-            this.length = length;
+            lengthValue = length;
         }
 
         #endregion
