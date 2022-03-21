1ceba11ab5c2311ecd092ef63ffbb38fefe1146c
DingBin.Li <lidingbin@live.com>
2022-03-21 14:35:25 +0800

    'modify'

---
+++ EZNEW/Extensions/EnumerationExtensions.cs
@@ -0,0 +1,35 @@
+﻿using EZNEW.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class EnumerationExtensions
+    {
+        /// <summary>
+        /// Gets enum dictionary
+        /// </summary>
+        /// <typeparam name="TEnum">Enum type</typeparam>
+        /// <param name="enumValue">Enum value</param>
+        /// <param name="displayFriendly">Display friendly</param>
+        /// <returns>Return a dictionary</returns>
+        public static Dictionary<int, string> GetEnumDictionary<TEnum>(this TEnum enumValue, bool displayFriendly = true) where TEnum : struct, Enum
+        {
+            return enumValue.GetType().GetEnumValueAndNames(displayFriendly);
+        }
+
+        /// <summary>
+        /// Gets enum key&value collection
+        /// </summary>
+        /// <typeparam name="TEnum">Enum type</typeparam>
+        /// <param name="enumValue">Enum value</param>
+        /// <param name="displayFriendly">Display friendly</param>
+        /// <returns>Return a key&value collection</returns>
+        public static KeyValueCollection<int, string> GetEnumKeyValueCollection<TEnum>(this TEnum enumValue, bool displayFriendly = true) where TEnum : struct, Enum
+        {
+            return new KeyValueCollection<int, string>(enumValue.GetEnumDictionary(displayFriendly).Select(c => c));
+        }
+    }
+}
---
+++ EZNEW/Model/KeyValueCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EZNEW.Model
+{
+    /// <summary>
+    /// Defines key-value collection
+    /// </summary>
+    public class KeyValueCollection<TKey, TValue> : List<KeyValuePair<TKey, TValue>>
+    {
+        public KeyValueCollection() : base() { }
+
+        public KeyValueCollection(IEnumerable<KeyValuePair<TKey, TValue>> collection) : base(collection) { }
+
+        public KeyValueCollection(int capacity) : base(capacity) { }
+
+        /// <summary>
+        /// Gets an empty collection
+        /// </summary>
+        /// <returns></returns>
+        public static KeyValueCollection<TKey, TValue> Empty()
+        {
+            return new KeyValueCollection<TKey, TValue>(0);
+        }
+    }
+}
