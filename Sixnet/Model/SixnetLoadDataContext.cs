39ad828ddb4625bcec2c94e93bf7b44082e18cf2
Li.DingBin <lidingbing@laoken.com>
2024-06-21 14:15:31 +0800

    'modify'

--- Sixnet/Model/SixnetLoadDataContext.cs
+++ Sixnet/Model/SixnetLoadDataContext.cs
@@ -4,19 +4,44 @@ using System.Text;
 
 namespace Sixnet.Model
 {
+    public interface ISixnetLoadDataContext<out T>
+    {
+        /// <summary>
+        /// Gets or sets the loadable
+        /// </summary>
+        ISixnetLoadable Loadable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the source datas
+        /// </summary>
+        IEnumerable<T> GetSourceDatas();
+    }
+
     /// <summary>
     /// Load data context
     /// </summary>
-    public class SixnetLoadDataContext<T>
+    public class SixnetLoadDataContext<T> : ISixnetLoadDataContext<T>
     {
+        private readonly IEnumerable<T> _sourceDatas;
+
+        public SixnetLoadDataContext(IEnumerable<T> sourceDatas, ISixnetLoadable loadable = null)
+        {
+            _sourceDatas = sourceDatas;
+            Loadable = loadable;
+        }
+
         /// <summary>
         /// Gets or sets the loadable
         /// </summary>
         public ISixnetLoadable Loadable { get; set; }
 
         /// <summary>
-        /// Gets or sets the source datas
+        /// Get source datas
         /// </summary>
-        public IEnumerable<T> SourceDatas { get; set; }
+        /// <returns></returns>
+        public IEnumerable<T> GetSourceDatas()
+        {
+            return _sourceDatas;
+        }
     }
 }
