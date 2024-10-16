aa69bfd55bf27aaadb4722c40f7551a684df4d7d
Li.DingBin <lidingbing@laoken.com>
2024-10-16 16:31:44 +0800

    'modify'

---
+++ Sixnet/Serialization/Json/Converter/FileFullPathJsonConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Sixnet.IO.FileAccess;
+
+namespace Sixnet.Serialization.Json.Converter
+{
+    public sealed class FileFullPathJsonConverter : JsonConverter<string>
+    {
+        readonly string fileObjectName;
+
+        private FileFullPathJsonConverter(string fileObjectName)
+        {
+            this.fileObjectName = fileObjectName;
+        }
+
+        public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return reader.GetString();
+        }
+
+        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(SixnetFileAccessor.GetFileFullPath(fileObjectName, value));
+        }
+
+        public static FileFullPathJsonConverter GetInstance(string fileObjectName)
+        {
+            return new FileFullPathJsonConverter(fileObjectName);
+        }
+    }
+}
---
+++ Sixnet/Serialization/Json/FileFullPathAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text.Json.Serialization;
+using Sixnet.Serialization.Json.Converter;
+
+namespace Sixnet.Serialization.Json
+{
+    /// <summary>
+    /// File full path
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class FileFullPathAttribute : JsonConverterAttribute
+    {
+        static readonly ConcurrentDictionary<string, FileFullPathJsonConverter> _converters = new();
+
+        public string FileObjectName { get; set; }
+
+        public FileFullPathAttribute(string fileObjectName = "")
+        {
+            FileObjectName = fileObjectName;
+        }
+
+        public override JsonConverter CreateConverter(Type typeToConvert)
+        {
+            return _converters.GetOrAdd(FileObjectName, k => FileFullPathJsonConverter.GetInstance(k));
+        }
+    }
+}
