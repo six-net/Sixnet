de58e2f59e7a661b28f7747d183331b0c43f582f
Li.DingBin <lidingbing@laoken.com>
2026-02-02 16:39:03 +0800

    'modify'

---
+++ Sixnet/Model/SixnetStringId.cs
@@ -0,0 +1,18 @@
+﻿// "Company © 2025. All rights reserved."
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sixnet.Model
+{
+    public class SixnetStringId
+    {
+        /// <summary>
+        /// Gets or sets the Id
+        /// </summary>
+        public string Id { get; set; }
+    }
+}
